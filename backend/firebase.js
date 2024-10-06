bfe93acabb12910cde0e83fc04c0ab193b182e2e
Pradeepreddybudida <137590716+Pradeepreddybudida@users.noreply.github.com>
2024-10-06 14:50:02 +0530

    Add files via upload

---
+++ backend/firebase.js
@@ -0,0 +1,13 @@
+// backend/firebase.js
+const admin = require('firebase-admin');
+const serviceAccount = JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON);
+
+if (!admin.apps.length) {
+    admin.initializeApp({
+        credential: admin.credential.cert(serviceAccount),
+    });
+}
+
+const db = admin.firestore();
+
+module.exports = db;
